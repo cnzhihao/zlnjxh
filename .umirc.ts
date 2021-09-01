1fed3043bba711e39c6699dbc7eba17c9b3d70c7
shanwai <shanwai@wacai.com>
2021-09-01 11:53:42 +0800

    init

--- .umirc.ts
+++ .umirc.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   title: 'zlnjxh-pro',
-  favicon: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
   mode: 'site',
   // more config: https://d.umijs.org/config
 });
+
+export default {
+  base: '/zlnjxh/',
+  publicPath: '/zlnjxh/',
+};
