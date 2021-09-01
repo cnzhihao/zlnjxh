dbe7ba4835f266d3992e4c329f7debaaaa0ca14c
shanwai <shanwai@wacai.com>
2021-09-01 11:07:45 +0800

    init

--- dist/config/config.d.ts
+++ dist/config/config.d.ts
@@ -1,10 +1,5 @@
 declare const _default: {
-  menus: {
-    '/\u52A0\u5165\u6211\u4EEC': {
-      title: string;
-      path: string;
-      children: string[];
-    }[];
-  };
+  base: string;
+  publicPath: string;
 };
 export default _default;
